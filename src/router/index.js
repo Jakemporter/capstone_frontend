7d1a74ec4ddd157b54842fc12b543bbfbb2c0802
Jake Porter <Jake.m.porter@gmail.com>
2020-11-03 19:50:14 -0600

    got rid of home and about pages that were empty

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 import CarsIndex from "../views/CarsIndex.vue";
 import CarsShow from "../views/CarsShow.vue";
 import CarsNew from "../views/CarsNew.vue";
@@ -12,19 +11,6 @@ import Logout from "../views/Logout.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
   { path: "/cars", name: "cars-index", component: CarsIndex},
   { path: "/cars/new", name: "cars-new", component: CarsNew},
   { path: "/cars/:id", name: "cars-show", component: CarsShow},
