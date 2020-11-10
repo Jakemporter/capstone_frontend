b68d34a57d173f1e4d4d8cc049300d40fe086f01
Jake Porter <Jake.m.porter@gmail.com>
2020-11-09 21:24:14 -0600

    added logo need to find way to not cover up body

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="">Car Auction</a>
+      <img id="logo" src="./assets/logo.png">
+      <a class="navbar-brand" href="/cars">Car Auction</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -42,7 +43,9 @@
 </template>
 
 <style>
-
+#logo {
+  width: 100px;
+}
 </style>
 
 <script>
