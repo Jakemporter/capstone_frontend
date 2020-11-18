467f8ec6387eab017630f199dd89d75f3ed4943f
Jake Porter <Jake.m.porter@gmail.com>
2020-11-18 16:46:55 -0600

    finally added background image

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,58 @@
 <template>
   <div id="app">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <!-- <img id="logo" src="./assets/logo.png"> -->
-      <a class="navbar-brand" href="/cars">Car Auction</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/cars">All Cars
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a v-if="isLoggedIn()" class="nav-link" href="/cars/new">New Car</a>
-          </li>
-          <li v-if="isLoggedIn()" class="nav-item">
-            <a class="nav-link" href="/bids">Your Bids</a>
-          </li>
-          <li v-if="!isLoggedIn()" class="nav-item">
-            <a class="nav-link" href="/signup">Signup</a>
-          </li>
-          <li v-if="!isLoggedIn()" class="nav-item">
-            <a class="nav-link" href="/login">Login</a>
-          </li>
-          <li v-if="isLoggedIn()" class="nav-item">
-            <a class="nav-link" href="/logout">Logout</a>
-          </li>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <!-- <img id="logo" src="./assets/logo.png"> -->
+        <a class="navbar-brand" href="/cars">Car Auction</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/cars">
+                All Cars
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a v-if="isLoggedIn()" class="nav-link" href="/cars/new">New Car</a>
+            </li>
+            <li v-if="isLoggedIn()" class="nav-item">
+              <a class="nav-link" href="/bids">Your Bids</a>
+            </li>
+            <li v-if="!isLoggedIn()" class="nav-item">
+              <a class="nav-link" href="/signup">Signup</a>
+            </li>
+            <li v-if="!isLoggedIn()" class="nav-item">
+              <a class="nav-link" href="/login">Login</a>
+            </li>
+            <li v-if="isLoggedIn()" class="nav-item">
+              <a class="nav-link" href="/logout">Logout</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
-  <div class="container">
-    <router-view/>
-  </div>
-      <footer class="py-5 bg-dark">
     <div class="container">
-      <div class="alert alert-success" v-if="flashMessage" v-on:click="flashMessage = ''">{{ flashMessage }}</div>
-      <p class="m-0 text-center text-white">Copyright &copy; Car Auction 2020</p>
+      <router-view />
     </div>
-    <!-- /.container -->
-  </footer>
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <div class="alert alert-success" v-if="flashMessage" v-on:click="flashMessage = ''">{{ flashMessage }}</div>
+        <p class="m-0 text-center text-white">Copyright &copy; Car Auction 2020</p>
+      </div>
+      <!-- /.container -->
+    </footer>
   </div>
 </template>
 
@@ -55,13 +64,13 @@
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
       flashMessage: "",
     };
   },
   methods: {
-    isLoggedIn: function () {
+    isLoggedIn: function() {
       return localStorage.getItem("jwt");
     },
   },
